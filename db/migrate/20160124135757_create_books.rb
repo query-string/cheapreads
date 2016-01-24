558b7a4507d4a80cfaa9df1f0dbc0e5bf1620fa4
Alex Timofeev <atimofeev@reactant.ru>
2016-01-24 21:00:29 +0700

    Establish Book model

---
+++ db/migrate/20160124135757_create_books.rb
@@ -0,0 +1,25 @@
+class CreateBooks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :books do |t|
+      t.integer :uid, nil: false
+      t.string :isbn
+      t.string :title, nil: false
+      t.text :link
+      t.text :description
+      t.integer :average_rating
+      t.integer :ratings_count
+      t.string :image
+      t.integer :publication_year
+      t.datetime :date_added
+      t.string :author_name
+      t.string :author_link
+
+      t.timestamps
+    end
+    add_index :books, :uid
+    add_index :books, :title
+    add_index :books, :average_rating
+    add_index :books, :ratings_count
+    add_index :books, :publication_year
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123045538) do
+ActiveRecord::Schema.define(version: 20160124135757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,28 @@ ActiveRecord::Schema.define(version: 20160123045538) do
     t.string   "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.integer  "uid"
+    t.string   "isbn"
+    t.string   "title"
+    t.text     "link"
+    t.text     "description"
+    t.integer  "average_rating"
+    t.integer  "ratings_count"
+    t.string   "image"
+    t.integer  "publication_year"
+    t.datetime "date_added"
+    t.string   "author_name"
+    t.string   "author_link"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["average_rating"], name: "index_books_on_average_rating", using: :btree
+    t.index ["publication_year"], name: "index_books_on_publication_year", using: :btree
+    t.index ["ratings_count"], name: "index_books_on_ratings_count", using: :btree
+    t.index ["title"], name: "index_books_on_title", using: :btree
+    t.index ["uid"], name: "index_books_on_uid", using: :btree
   end
 
 end
