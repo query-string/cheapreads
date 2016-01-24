fc9461688b2fd35f417d1cb68a48eb6dd89e06ae
Alex Timofeev <atimofeev@reactant.ru>
2016-01-24 20:13:32 +0700

    Pull the most important book's information

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def index
     client = Goodreads::Client.new(api_key: ENV["GOODREADS_KEY"], api_secret: ENV["GOODREADS_SECRET"])
-    shelf  = client.shelf(current_authentication.uid, "Want to read")
+    shelf  = client.shelf(current_authentication.uid, "to-read", {per_page: 200})
     p shelf.total
     @books = shelf.books
   end
