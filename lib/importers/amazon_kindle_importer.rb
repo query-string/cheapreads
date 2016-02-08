71b138f82d59e668d56c43cad152ae2c0c1cb546
Alex Timofeev <atimofeev@reactant.ru>
2016-02-08 07:44:15 +0700

    Implement Amazon Kindle price parser

--- lib/importers/amazon_kindle_importer.rb
+++ lib/importers/amazon_kindle_importer.rb
@@ -1,7 +1,36 @@
 class AmazonKindleImporter
-  attr_reader :uid
+  require "nokogiri"
+  require "open-uri"
 
-  def initialize(uid)
-    @uid = uid
+  attr_reader :isbn
+
+  def initialize(isbn)
+    @isbn = isbn
+  end
+
+  def price
+    kindle.text
+  end
+
+  private
+
+  def url
+    "http://www.amazon.com/gp/product/#{isbn}"
+  end
+
+  def randomize_chrome_version
+    "Chrome/#{rand(17...48)}.0.#{rand(2000...2564)}.109"
+  end
+
+  def page
+    @page ||= Nokogiri::HTML(open(url, "User-Agent" => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) #{randomize_chrome_version} Safari/11601.4.4"))
+  end
+
+  def buttons
+    page.css("#twister .top-level .dp-price-col .a-color-price")
+  end
+
+  def kindle
+    buttons.first
   end
 end
