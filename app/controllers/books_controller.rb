f08f464f77b892b266bdfe5d947965dd0ee48c62
Alex Timofeev <atimofeev@reactant.ru>
2016-03-16 07:45:36 +0700

    Add new arrivals to the person list

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,15 +7,26 @@ class BooksController < ApplicationController
 
   def create
     client  = Goodreads::Client.new(api_key: ENV["GOODREADS_KEY"], api_secret: ENV["GOODREADS_SECRET"])
-    shelf   = client.shelf(current_authentication.uid, "to-read", {per_page: 200})
+    shelf   = client.shelf(current_authentication.uid, "to-read", {per_page: 200}).books
 
-    gr_shelf     = shelf.books.map { |g| g.book.id }
-    local_shelf  = current_authentication.books.map(&:uid)
-    new_arrivals = gr_shelf - local_shelf
-    read_already = local_shelf - gr_shelf
+    # Calculate the new arrivals and books read already
+    goodreads_ids = shelf.map { |g| g.book.id }
+    local_ids     = current_authentication.books.map(&:uid)
+    new_arrivals  = goodreads_ids - local_ids
+    read_already  = local_ids - goodreads_ids
 
-    # Remove already read books from the list
+    # Add new arrivals to the person list
+    if new_arrivals.any?
+      shelf.select { |g| new_arrivals.include?(g.book.id) }.each do |item|
+        book = Book.import(item, current_authentication)
+        book.synchronize
+      end
+    end
+
+    # Remove already read books from the person list
     Book.where("uid IN (?)", read_already).each { |book| current_authentication.books.delete(book) } if read_already.any?
+
+    redirect_to root_path, flash: { notice: "Synchronized!" }
   end
 
   def creates
